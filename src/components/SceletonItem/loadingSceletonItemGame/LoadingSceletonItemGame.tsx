ca1246f3092ddfaa23de448866c56cc6e3120164
tolanchik1987 <tolanchik1987@mail.ru>
2022-11-04 11:59:14 +0400

    add sceleton ts

--- src/components/SceletonItem/loadingSceletonItemGame/LoadingSceletonItemGame.tsx
+++ src/components/SceletonItem/loadingSceletonItemGame/LoadingSceletonItemGame.tsx
@@ -1,18 +1,16 @@
-import React, { memo } from "react";
+import React from "react";
 import ContentLoader from "react-content-loader";
 import classes from "./LoadingSceletonItemGame.module.scss";
 
-const LoadingSceletonItemGame = memo((props) => (
-   <ContentLoader
-      className={classes.conteiner__game}
-      speed={1}
-      width={389}
-      height={499}
-      viewBox="0 0 389 499"
-      backgroundColor="#404040"
-      foregroundColor="#dbdbdb"
-      {...props}
-   >
+const LoadingSceletonItemGame = React.memo((props) => (
+   <ContentLoader    className={classes.conteiner__game}
+   speed={1}
+   width={389}
+   height={499}
+   viewBox="0 0 389 499"
+   backgroundColor="#404040"
+   foregroundColor="#dbdbdb"
+   {...props}>
       <rect x="0" y="0" rx="10" ry="10" width="385" height="229" />
       <rect x="0" y="285" rx="10" ry="10" width="62" height="20" />
       <rect x="80" y="285" rx="10" ry="10" width="120" height="20" />
@@ -21,6 +19,6 @@ const LoadingSceletonItemGame = memo((props) => (
       <rect x="232" y="460" rx="25" ry="25" width="153" height="38" />
       <rect x="0" y="249" rx="10" ry="10" width="180" height="24" />
    </ContentLoader>
-))
+));
 
 export default LoadingSceletonItemGame;
