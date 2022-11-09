38d9b205eea2d68bebf720e16c8acd0d58297c39
tolanchik1987 <tolanchik1987@mail.ru>
2022-11-09 15:27:07 +0400

    add 404.html

--- src/components/utils/calcTotalPrice.ts
+++
@@ -1,9 +0,0 @@
-//import { useDispatch } from "react-redux";
-import { setTotalPrice } from "../store/cartReducer/cartSlice";
-import { GameType } from "../types/type";
-
-//const dispatch = useDispatch()
-const calcTotalPrice = (items: GameType[]) => {
- // return  dispatch(setTotalPrice(items.reduce((acc, game) => acc + game.price, 0)));
-}
-export default calcTotalPrice;
